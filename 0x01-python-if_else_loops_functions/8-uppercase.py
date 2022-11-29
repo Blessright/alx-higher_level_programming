24dfe4f7253cacac492473b8f02d041cca1d478d
Bolaji Blessing <smarteematics@gmail.com>
2022-11-29 15:59:33 -0800

    task 1 and 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
-
 def uppercase(str):
     for i in range(len(str)):
-        uni_code = ord(str[i])
-        if uni_code >= 97 and uni_code < 123:
+        if ord(str[i]) >= 97 and ord(str[i]) < 123:
             letter = 32
         else:
             letter = 0
-            print("{:c}".format(uni_code) - letter, end='')
+            print("{:c}".format(ord(str[i]) - letter), end='')
             print()
